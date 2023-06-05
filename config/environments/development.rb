d1ba54608facfce268ccfe0bea965658de9c4f19
Rodrigo Toledo <rodrigo@rtoledo.inf.br>
2023-06-04 21:49:49 -0300

    add whitelist

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,4 +1,4 @@
-require "active_support/core_ext/integer/time"
+require 'active_support/core_ext/integer/time'
 
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
@@ -19,13 +19,13 @@ Rails.application.configure do
 
   # Enable/disable caching. By default caching is disabled.
   # Run rails dev:cache to toggle caching.
-  if Rails.root.join("tmp/caching-dev.txt").exist?
+  if Rails.root.join('tmp/caching-dev.txt').exist?
     config.action_controller.perform_caching = true
     config.action_controller.enable_fragment_cache_logging = true
 
     config.cache_store = :memory_store
     config.public_file_server.headers = {
-      "Cache-Control" => "public, max-age=#{2.days.to_i}"
+      'Cache-Control' => "public, max-age=#{2.days.to_i}"
     }
   else
     config.action_controller.perform_caching = false
@@ -71,12 +71,12 @@ Rails.application.configure do
   config.hosts << '0.0.0.0/0'
   config.hosts << '::/0'
   config.hosts << /.*\.ngrok\.io/
-  config.hosts << /[a-z0-9\-]+\.ngrok\-free\.app/
+  config.hosts << /[a-z0-9-]+\.ngrok-free\.app/
   config.action_mailer.default_url_options = { host: 'localhost', only_path: false }
   config.action_mailer.delivery_method = :smtp
 
   config.action_mailer.delivery_method = :letter_opener_web
   config.action_mailer.perform_deliveries = true
-  config.web_console.whitelisted_ips = '2804:4308:546:0:b413:6d09:6220:826b'  # Substitua pelo seu endereço IP permitido
-  config.default_url_options = { host: ENV['API_BASE_URL'] }
+  config.web_console.whitelisted_ips = ['2804:4308:546:0:2ce6:f687:928c:980f', '2804:4308:546:0:b413:6d09:6220:826b']  # Substitua pelo seu endereço IP permitido
+  config.default_url_options = { host: ENV.fetch('API_BASE_URL', nil) }
 end
